e74104caa6c21e9e7d2b193b467643e6ece03c2b
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-09-15 13:15:50 +0600

    feat: user model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
